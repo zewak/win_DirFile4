d9428ca4e9747b1c7b03e981254af023f19ea465
zewak41@outlook.com <zewak41@outlook.com>
2021-09-22 13:52:44 +0200

    i co ?

--- Form1.cs
+++ Form1.cs
@@ -8,8 +8,26 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Win_DirFile4
 {
+        public class WeatherForecast
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string Summary { get; set; }
+        public string SummaryField;
+        public IList<DateTimeOffset> DatesAvailable { get; set; }
+        public Dictionary<string, HighLowTemps> TemperatureRanges { get; set; }
+        public string[] SummaryWords { get; set; }
+    }
+
+    public class HighLowTemps
+    {
+        public int High { get; set; }
+        public int Low { get; set; }
+    } 
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -31,7 +49,27 @@ namespace Win_DirFile4
             LB_Files1.Items.Add("Drugi");
             LB_Files1.Items.Add("Trzeci");
 
+  
+
             label1.Text = "Przeczytano";
+
+            // test z pracy na podstawie master dfdf
+
+                        var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Hot",
+                SummaryField = "Hot",
+                DatesAvailable = new List<DateTimeOffset>() 
+                    { DateTime.Parse("2019-08-01"), DateTime.Parse("2019-08-02") },
+                TemperatureRanges = new Dictionary<string, HighLowTemps>
+                    {
+                        ["Cold"] = new HighLowTemps { High = 20, Low = -10 },
+                        ["Hot"] = new HighLowTemps { High = 60 , Low = 20 }
+                    },
+                SummaryWords = new[] { "Cool", "Windy", "Humid" }
+            };
         }
     }
 }
